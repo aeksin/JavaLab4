ea2c76be225aa2ee7435f9f002b7e9b05792697c
aeksin <113206813+aeksin@users.noreply.github.com>
2024-05-18 21:45:19 +0300

    reformat code

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicBoolean;
+
 import static java.lang.Thread.sleep;
 
 public class Elevator implements Runnable {
@@ -11,15 +12,17 @@ public class Elevator implements Runnable {
         this.kill = kill;
     }
 
-    enum directions{
+    enum directions {
         DOWN,
         UP,
         STAYING;
+
         @Override
         public String toString() {
             return name().toLowerCase();
         }
     }
+
     public int[] destinations;
     public int in;
     private directions direction;
@@ -28,19 +31,24 @@ public class Elevator implements Runnable {
     private int numberOfElevator;
     private AtomicBoolean called;
     private AtomicBoolean kill;
+
     public void setFrom(int from) {
         this.from = from;
     }
+
     public void setCalled(AtomicBoolean called) {
         this.called = called;
     }
+
     public AtomicBoolean getCalled() {
         return called;
     }
+
     public int getCurrentFloor() {
         return this.currentFloor;
     }
-    Elevator(int height,int number){
+
+    Elevator(int height, int number) {
         currentFloor = 0;
         called = new AtomicBoolean(false);
         direction = directions.STAYING;
@@ -48,108 +56,105 @@ public class Elevator implements Runnable {
         kill = new AtomicBoolean(false);
         this.numberOfElevator = number;
     }
-    public boolean isStaying(){
+
+    public boolean isStaying() {
         return direction == directions.STAYING;
     }
 
     @Override
-    public void run(){
-        while (true){
-            if (getKill().get()){
+    public void run() {
+        while (true) {
+            if (getKill().get()) {
                 break;
             }
-            if (isStaying() && getCalled().get()){
+            if (isStaying() && getCalled().get()) {
                 called = new AtomicBoolean(false);
-                try{
-                    System.out.println("elevator №"+numberOfElevator+" called from "+currentFloor+" to "+from);
-                    if (currentFloor<from){
+                try {
+                    System.out.println("elevator №" + numberOfElevator + " called from " + currentFloor + " to " + from);
+                    if (currentFloor < from) {
                         direction = directions.UP;
-                        while (currentFloor<from){
+                        while (currentFloor < from) {
                             Thread.sleep(1000);
                             currentFloor++;
-                            System.out.println("elevator №"+numberOfElevator+" going up "+currentFloor);
+                            System.out.println("elevator №" + numberOfElevator + " going up " + currentFloor);
                         }
-                    }
-                    else if (currentFloor >from){
+                    } else if (currentFloor > from) {
                         direction = directions.DOWN;
-                        while (currentFloor>from){
+                        while (currentFloor > from) {
                             sleep(1000);
                             currentFloor--;
-                            System.out.println("elevator №"+numberOfElevator+" going down "+currentFloor);
+                            System.out.println("elevator №" + numberOfElevator + " going down " + currentFloor);
                         }
                     }
 
-                    synchronized (ElevatorSimulation.PeopleOnFloor){
+                    synchronized (ElevatorSimulation.PeopleOnFloor) {
                         synchronized (ElevatorSimulation.PeopleOnFloor.get(currentFloor)) {
-                            if (ElevatorSimulation.PeopleOnFloor.get(currentFloor).isEmpty()){
-                                System.out.println("elevator №"+numberOfElevator+" arrived at empty floor");
+                            if (ElevatorSimulation.PeopleOnFloor.get(currentFloor).isEmpty()) {
+                                System.out.println("elevator №" + numberOfElevator + " arrived at empty floor");
                                 direction = directions.STAYING;
                                 continue;
                             }
                             int to = ElevatorSimulation.PeopleOnFloor.get(currentFloor).peek();
-                            if (to<from){
+                            if (to < from) {
                                 direction = directions.DOWN;
-                            }
-                            else{
+                            } else {
                                 direction = directions.UP;
                             }
                             takePeopleFromFloor();
                         }
-                        while (in != 0){
-                            if (direction == directions.DOWN){
+                        while (in != 0) {
+                            if (direction == directions.DOWN) {
                                 sleep(1000);
                                 currentFloor--;
-                                if (destinations[currentFloor]>0){
+                                if (destinations[currentFloor] > 0) {
                                     in -= destinations[currentFloor];
                                     destinations[currentFloor] = 0;
                                 }
                                 takePeopleFromFloor();
                             }
-                            if (direction == directions.UP){
+                            if (direction == directions.UP) {
                                 sleep(1000);
                                 currentFloor++;
-                                if (destinations[currentFloor]>0){
+                                if (destinations[currentFloor] > 0) {
                                     in -= destinations[currentFloor];
                                     destinations[currentFloor] = 0;
                                 }
                                 takePeopleFromFloor();
                             }
-                            System.out.println("elevator №"+numberOfElevator+" moving "+direction+", currently on "+currentFloor+" floor with "+in+" people in elevator");
+                            System.out.println("elevator №" + numberOfElevator + " moving " + direction + ", currently on " + currentFloor + " floor with " + in + " people in elevator");
                         }
                         direction = directions.STAYING;
                     }
 
-                }
-                catch (Exception e){
+                } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
 
             }
         }
     }
-    public void takePeopleFromFloor(){
-        synchronized (ElevatorSimulation.PeopleOnFloor){
-            synchronized (ElevatorSimulation.PeopleOnFloor.get(currentFloor)){
-                ArrayList<Integer> staying = new ArrayList<Integer>();
-                for (Integer to:ElevatorSimulation.PeopleOnFloor.get(currentFloor)){
 
-                    if (to<currentFloor && direction == directions.DOWN){
+    public void takePeopleFromFloor() {
+        synchronized (ElevatorSimulation.PeopleOnFloor) {
+            synchronized (ElevatorSimulation.PeopleOnFloor.get(currentFloor)) {
+                ArrayList<Integer> staying = new ArrayList<Integer>();
+                for (Integer to : ElevatorSimulation.PeopleOnFloor.get(currentFloor)) {
+
+                    if (to < currentFloor && direction == directions.DOWN) {
                         destinations[to]++;
-                        System.out.println("man gets into the elevator №"+numberOfElevator+" to go from "+currentFloor+" to "+to);
+                        System.out.println("man gets into the elevator №" + numberOfElevator + " to go from " + currentFloor + " to " + to);
                         in++;
-                    }
-                    else if (to>currentFloor && direction == directions.UP){
+                    } else if (to > currentFloor && direction == directions.UP) {
                         destinations[to]++;
-                        System.out.println("man gets into the elevator №"+numberOfElevator+" to go from "+currentFloor+" to "+to);
+                        System.out.println("man gets into the elevator №" + numberOfElevator + " to go from " + currentFloor + " to " + to);
                         in++;
-                    }
-                    else{
+                    } else {
                         staying.add(to);
                     }
 
                 }
                 ElevatorSimulation.PeopleOnFloor.get(currentFloor).clear();
-                for (int person: staying){
+                for (int person : staying) {
                     ElevatorSimulation.PeopleOnFloor.get(currentFloor).add(person);
                 }
             }
--- src/Pair.java
+++ src/Pair.java
@@ -3,7 +3,8 @@ import java.util.Comparator;
 public class Pair<T1, T2> {
     private T1 first;
     private T2 second;
-    Pair(T1 first, T2 second){
+
+    Pair(T1 first, T2 second) {
         this.first = first;
         this.second = second;
     }
@@ -23,14 +24,16 @@ public class Pair<T1, T2> {
     public void setFirst(T1 start) {
         this.first = start;
     }
+
     @Override
-    public String toString(){
-        return this.first+" "+this.second;
+    public String toString() {
+        return this.first + " " + this.second;
     }
 }
-class SortBySecond implements Comparator<Pair<Integer,Integer>> {
 
-    public int compare(Pair<Integer,Integer> first, Pair<Integer,Integer> second) {
-        return first.getSecond()-second.getSecond();
+class SortBySecond implements Comparator<Pair<Integer, Integer>> {
+
+    public int compare(Pair<Integer, Integer> first, Pair<Integer, Integer> second) {
+        return first.getSecond() - second.getSecond();
     }
 }
